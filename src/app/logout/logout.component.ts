5cf7bc34fe72f1152f427b3fd12ccced204b8fc6
Bas <doppleb@gmail.com>
2020-02-13 14:06:34 +0100

    Final changes to docs

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) { }
+  ngOnDestroy(){
+  window.location.reload(); 
   }
-
+  ngOnInit() {
+    this.goNext();
+  }
+  goNext(){
+   
+    setTimeout(() => {
+        this.router.navigate(['home'])
+      }, 1000);
+}
 }
